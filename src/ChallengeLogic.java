b375011c3c0cadbd791268a5725a45b94909faf1
Hejociara <hejociara@gmail.com>
2022-09-05 22:37:25 -0300

    "Feature"

--- src/ChallengeLogic.java
+++ src/ChallengeLogic.java
@@ -2,6 +2,15 @@ public class ChallengeLogic {
     public static void main(String[] args) {
         boolean job1 = true;
         boolean job2 = false;
-        System.out.println(" ");
+
+        boolean boughtTv50 = job1 && job2;
+        boolean boughtTv32 = job1 || job2;
+        boolean boughtIceCream = job1 || job2;
+
+        System.out.println("Bought 50 inch television? " + boughtTv50);
+        System.out.println("Bought 32 inch television? " + boughtTv32);
+        System.out.println("Bought Ice cream? " + boughtIceCream);
+        System.out.println("It's healthy? " + !boughtIceCream);
+
     }
 }
